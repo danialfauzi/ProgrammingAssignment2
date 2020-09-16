a862c3bf779494798cfbccbbf2456a6b56262606
Danial <danialfauzi@outlook.com>
2020-09-16 15:02:40 +0700

    a little chage.

--- cachematrix.R
+++ cachematrix.R
@@ -28,7 +28,6 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   inv <- x$getInverse()
   if (!is.null(inv)) {
-    message("getting cached data")
     return(inv)
   }
   mat <- x$get()
